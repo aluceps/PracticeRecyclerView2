b3866a3a7d3eaa1e81de89469b7bfc67f4769c83
atsushi.takano <atsushi.takano@every.tv>
2022-06-24 16:03:13 +0900

    ViewBinding を有効化

--- app/src/main/java/me/aluceps/practicerecyclerview/MainActivity.kt
+++ app/src/main/java/me/aluceps/practicerecyclerview/MainActivity.kt
@@ -2,10 +2,16 @@ package me.aluceps.practicerecyclerview
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import me.aluceps.practicerecyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
